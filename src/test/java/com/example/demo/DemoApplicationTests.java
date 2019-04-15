d92ca4c53116786917a182539704c734963d55b5
xiaozhuanfeng <627794853@qq.com>
2019-04-15 16:18:03 +0800

    常用模板

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.apache.juli.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+    @Test
+    public void contextLoads() {
+        logger.info("----->");
+    }
+
+}
